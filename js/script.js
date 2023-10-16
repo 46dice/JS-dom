d2be73598fa87605a8570b7af71f2a76d7aa118b
Артурик <artursa212@mail.ru>
2023-10-16 14:36:58 +0500

    Отработка слушателей событий

--- js/script.js
+++ js/script.js
@@ -14,46 +14,92 @@
 
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Одержимость",
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Скотт Пилигрим против...",
-    ]
-};
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            "Одержимость",
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Скотт Пилигрим против...",
+        ]
+    };
 
-//1
-const promo = document.querySelector('.promo__adv');
-const img = promo.querySelectorAll('img');
-img.forEach(element => {
-    element.remove()
+    const promo = document.querySelector('.promo__adv');
+    const img = promo.querySelectorAll('img');
+    const genre = document.querySelector('.promo__genre');
+    const bg = document.querySelector('.promo__bg');
+    const interactiveList = document.querySelector('.promo__interactive-list');
+    const activeBtn = document.querySelector('.promo__menu-item_active');
 
-});
+    //1
+    img.forEach(element => {
+        element.remove()
+    });
 
-//2
-const genre = document.querySelector('.promo__genre');
-genre.innerHTML = 'драма';
+    //2
+    genre.innerHTML = 'драма';
 
-//3
-const bg = document.querySelector('.promo__bg');
-bg.style.backgroundImage = `url('../img/bg.jpg')`;
+    //3
+    bg.style.backgroundImage = `url('../img/bg.jpg')`;
+    //4
 
-//4
-const interactiveList = document.querySelector('.promo__interactive-list');
-interactiveList.innerHTML = "";
 
-movieDB.movies.sort();
-movieDB.movies.forEach((film, i) => {
-    interactiveList.innerHTML += `
-        <li class="promo__interactive-item">${i + 1} ${film}
-            <div class="delete"></div>
-        </li>
-    `;
-});;
+    const sortArr = (arr) => arr.sort();
+    const deleteEl = (e) => e.target.remove();
 
-// interactiveList.push(...movieDB.movies.sort())
+    function createMovieList(films, parent) {
+        parent.innerHTML = "";
+        sortArr(films);
+
+        films.forEach((film, i) => {
+            parent.innerHTML += `
+                <li class="promo__interactive-item">${i + 1} ${film}
+                    <div class="delete"></div>
+                </li>
+            `;
+        });
+
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+
+                createMovieList(films, parent);
+            })
+        })
+    }
+
+    activeBtn.addEventListener('click', deleteEl);
+
+    createMovieList(movieDB.movies, interactiveList);
+    // -----------------------Отработка слушателей событий---------------------//
+
+    //1
+    const addForm = document.querySelector('.add');
+    const addInput = addForm.querySelector('input.adding__input');
+    const checkbox = addForm.querySelector('[type="checkbox"]');
+
+    addForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        let newFilm = addInput.value;
+        const favorite = checkbox.checked;
+
+        if (newFilm) {
+            if (newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0, 22)}...`;
+            }
+            movieDB.movies.push(newFilm);
+            sortArr(movieDB.movies);
+            createMovieList(movieDB.movies, interactiveList);
+        }
+
+        e.target.reset();
+    })
+})
+
+
 
 
 
